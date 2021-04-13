1d7c33696c04a59f195ee819d4d3ae4b11996682
iruy <jeon1310>
2021-04-13 20:14:24 +0900

    activity 완성

--- app/src/main/java/kr/hs/emirim/w2015/project4_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project4_2/MainActivity.java
@@ -3,12 +3,22 @@ package kr.hs.emirim.w2015.project4_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox checkStart;
+    RadioGroup rg;
+    ImageView imgv;
+    LinearLayout linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkStart = findViewById(R.id.start);
     }
 }
