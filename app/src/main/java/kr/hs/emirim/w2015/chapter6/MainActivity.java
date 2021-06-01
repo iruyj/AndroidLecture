6a54b781c280d816f2c06ebb9ec805b03ba0fbc8
iruy <jeon1310>
2021-06-01 19:41:32 +0900

    Commpletle 직접풀어보기 6_1 commit

--- app/src/main/java/kr/hs/emirim/w2015/chapter6/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/chapter6/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -18,7 +19,7 @@ import android.widget.TimePicker;
 public class MainActivity extends AppCompatActivity {
     Chronometer chrono1;
     RadioGroup rg;
-    CalendarView calendar;
+    DatePicker datePick;
     TimePicker time;
     TextView textResult;
     RadioButton radioCal, radioTime;
@@ -30,27 +31,24 @@ public class MainActivity extends AppCompatActivity {
 
         chrono1 = findViewById(R.id.chrono1);
         rg = findViewById(R.id.rg);
-        calendar = findViewById(R.id.calendar);
+        datePick = findViewById(R.id.datepick);
         time = findViewById(R.id.time);
         textResult = findViewById(R.id.text_result);
-        Button btnStart = findViewById(R.id.btn_start);
-        Button btnDone = findViewById(R.id.btn_done);
+        chrono1.setOnClickListener(chronoListener);
+        textResult.setOnLongClickListener(textListener);
 
         rg.setOnCheckedChangeListener(radioListener);
 
-        btnStart.setOnClickListener(btnListener);
-        btnDone.setOnClickListener(btnListener);
-
-        calendar.setOnDateChangeListener(calendarListener);
+        //calendar.setOnDateChangeListener(calendarListener);
     }
     RadioGroup.OnCheckedChangeListener radioListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
-            calendar.setVisibility(View.INVISIBLE);
+            datePick.setVisibility(View.INVISIBLE);
             time.setVisibility(View.INVISIBLE);
             switch (checkedId){
                 case R.id.radio_date:
-                    calendar.setVisibility(View.VISIBLE);
+                    datePick.setVisibility(View.VISIBLE);
                     break;
                 case R.id.radio_time:
                     time.setVisibility(View.VISIBLE);
@@ -59,7 +57,35 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    View.OnClickListener btnListener = new View.OnClickListener() {
+    // 예약 시작했을때
+    View.OnClickListener chronoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            chrono1.setBase(SystemClock.elapsedRealtime()); //avd에 있는 시계를 이용해 초단위로 사용
+            chrono1.start();
+            chrono1.setTextColor(Color.RED);
+            rg.setVisibility(View.VISIBLE);
+            datePick.setVisibility(View.VISIBLE);
+        }
+    };
+
+    // 예약 끝났을 때
+    View.OnLongClickListener textListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            chrono1.stop();
+            chrono1.setTextColor(Color.BLUE);
+            textResult.setText("  >> "+datePick.getYear() + "년" + (datePick.getMonth()+1) +"월 "+datePick.getDayOfMonth() +"일");
+            textResult.append(time.getCurrentHour()+"시 "+time.getCurrentMinute()+"분 예약완료됨");
+            rg.setVisibility(View.INVISIBLE);
+            datePick.setVisibility(View.INVISIBLE);
+            time.setVisibility(View.INVISIBLE);
+            return false;
+        }
+    };
+
+    //캘린더뷰일때는 변경되는 날짜를 가져오는 변수, 버튼이였는데 아니라서 없앰
+/*    View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()){
@@ -71,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btn_done:
                     chrono1.stop();
                     chrono1.setTextColor(Color.BLUE);
-                    textResult.setText(y + "년" + m +"월 "+d +"일");
+                    textResult.setText("  >> "+y + "년" + m +"월 "+d +"일");
                     textResult.append(time.getCurrentHour()+"시 "+time.getCurrentMinute()+"분 예약완료됨");
                     break;
             }
@@ -86,5 +112,5 @@ public class MainActivity extends AppCompatActivity {
             m = month;
             d = dayOfMonth;
         }
-    };
+    };*/
 }
