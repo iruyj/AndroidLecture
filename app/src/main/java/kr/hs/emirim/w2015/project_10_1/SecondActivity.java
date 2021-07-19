60f165350e1edfd76bbefbcf9dbe417923d68b5e
iruy <jeon1310>
2021-07-19 10:41:23 +0900

    Complete ThirdActivity.java

--- app/src/main/java/kr/hs/emirim/w2015/project_10_1/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project_10_1/SecondActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.w2015.project_10_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
@@ -12,6 +13,13 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button btn_close = findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(closeListener);
 
     }
+    View.OnClickListener closeListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
