351d514d2702f8252b6e9311eb2c80b4f54e02c4
iruy <jeon1310>
2021-07-19 10:52:44 +0900

    Complete MainActivity.java and ThirdActivity.java 수정

--- app/src/main/java/kr/hs/emirim/w2015/project_10_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project_10_1/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rg = findViewById(R.id.rg);
         Button btn_open = findViewById(R.id.btn_open);
         btn_open.setOnClickListener(btnOpenListener);
     }
--- app/src/main/java/kr/hs/emirim/w2015/project_10_1/ThirdActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project_10_1/ThirdActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.w2015.project_10_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
 
@@ -12,5 +13,13 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Button btnThirdclose = findViewById(R.id.btn_third_close);
+        btnThirdclose.setOnClickListener(btnThirdcloseListener);
     }
+    View.OnClickListener btnThirdcloseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
