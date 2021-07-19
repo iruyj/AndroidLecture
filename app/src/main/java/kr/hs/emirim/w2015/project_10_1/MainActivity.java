8c2fdfd9ae7520ab1d105a33297164f50ccc0384
iruy <jeon1310>
2021-07-19 09:54:04 +0900

    Completion MainActivity

--- app/src/main/java/kr/hs/emirim/w2015/project_10_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project_10_1/MainActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.w2015.project_10_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_open = findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(btnOpenListener);
     }
+    View.OnClickListener btnOpenListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
