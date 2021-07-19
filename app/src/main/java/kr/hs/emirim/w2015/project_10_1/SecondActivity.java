034354602412213f9ae568cb43b64934aa8d0e98
iruy <jeon1310>
2021-07-19 10:30:52 +0900

    Change activity_third.xml

--- app/src/main/java/kr/hs/emirim/w2015/project_10_1/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project_10_1/SecondActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.w2015.project_10_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button btn_close = findViewById(R.id.btn_close);
+
     }
 }
