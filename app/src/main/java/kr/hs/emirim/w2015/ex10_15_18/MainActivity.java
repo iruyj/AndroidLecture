f66f0bf46a731f54469437bd3017231efec0ae46
iruy <jeon1310>
2021-07-20 11:45:04 +0900

    Edit SecondActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/MainActivity.java
@@ -1,16 +1,41 @@
 package kr.hs.emirim.w2015.ex10_15_18;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1,edit2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.title_main);
 
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        Button btnplus = findViewById(R.id.btn_plus);
+
+        btnplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("num1", Integer.parseInt(edit1.getText().toString()));
+                intent.putExtra("num2", Integer.parseInt(edit2.getText().toString()));
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+    
+    // 세컨드에서 값을 받아오는 함수
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/SecondActivity.java
@@ -2,13 +2,33 @@ package kr.hs.emirim.w2015.ex10_15_18;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    int sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int num1 = intent.getIntExtra("num1",0);
+        int num2 = intent.getIntExtra("num2",0);
+        sum = num1+num2;
+
+        Button btn100 = findViewById(R.id.btn_back);
+        btn100.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent  = new Intent(getApplicationContext(), MainActivity.class);
+                mainIntent.putExtra("sum",sum);
+                setResult(RESULT_OK,mainIntent);
+                finish();
+            }
+        });
     }
 }
