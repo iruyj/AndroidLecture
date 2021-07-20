39eca9296c45708b654226b2805b77928c32a69c
iruy <jeon1310>
2021-07-20 13:41:04 +0900

    Competion 사칙연산 완성

--- app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/MainActivity.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1,edit2;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle(R.string.title_main);
 
+        rg = findViewById(R.id.rg);
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
-        Button btnplus = findViewById(R.id.btn_plus);
+        Button btncalc = findViewById(R.id.btn_calc);
 
-        btnplus.setOnClickListener(new View.OnClickListener() {
+        btncalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String cal="";
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                switch (rg.getCheckedRadioButtonId()){
+                    case R.id.rplus:
+                        cal = "+";
+                        break;
+                    case R.id.rminus:
+                        cal = "-";
+                        break;
+                    case R.id.rmulti:
+                        cal = "*";
+                        break;
+                    case R.id.rdivice:
+                        cal = "/";
+                        break;
+                }
+                intent.putExtra("calcul",cal);
                 intent.putExtra("num1", Integer.parseInt(edit1.getText().toString()));
                 intent.putExtra("num2", Integer.parseInt(edit2.getText().toString()));
                 startActivityForResult(intent,0);
@@ -34,13 +53,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     
-    // 세
+    // 세컨드에서 값 가져오기
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK ){
-            int sum = data.getIntExtra("sum",0);
-            Toast.makeText(getApplicationContext(),"합계 : "+sum, Toast.LENGTH_SHORT ).show();
+            int result = data.getIntExtra("result",0);
+            Toast.makeText(getApplicationContext(),"결과 : "+result, Toast.LENGTH_SHORT ).show();
         }
     }
 }
--- app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/ex10_15_18/SecondActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-    int sum;
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +18,29 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int num1 = intent.getIntExtra("num1",0);
         int num2 = intent.getIntExtra("num2",0);
-        sum = num1+num2;
+        String calcul = intent.getStringExtra("calcul");
+
+        switch (calcul){
+            case "+":
+                result = num1+num2;
+                break;
+            case "-":
+                result = num1-num2;
+                break;
+            case "*":
+                result = num1*num2;
+                break;
+            case "/":
+                result = num1/num2;
+                break;
+        }
 
         Button btn100 = findViewById(R.id.btn_back);
         btn100.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mainIntent  = new Intent(getApplicationContext(), MainActivity.class);
-                mainIntent.putExtra("sum",sum);
+                mainIntent.putExtra("result",result);
                 setResult(RESULT_OK,mainIntent);
                 finish();
             }
