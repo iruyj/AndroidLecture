b5d3480660d8250bb4d85a5a620dc89103258bc3
iruy <jeon1310>
2021-07-21 13:39:58 +0900

    Edit MainActivity.java Commit

---
+++ app/src/main/java/kr/hs/emirim/w2015/project_bmi/ResultActivity.java
@@ -0,0 +1,15 @@
+package kr.hs.emirim.w2015.project_bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
