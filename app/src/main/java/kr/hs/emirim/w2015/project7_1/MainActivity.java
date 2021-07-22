3700c12127de62208c0ccf243bdb82cba488fba4
iruy <jeon1310>
2021-07-22 10:11:16 +0900

    Edit MainActivity.java -> option event Commit

--- app/src/main/java/kr/hs/emirim/w2015/project7_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project7_1/MainActivity.java
@@ -1,10 +1,13 @@
 package kr.hs.emirim.w2015.project7_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -27,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
          MenuInflater inflater = getMenuInflater();
          inflater.inflate(R.menu.menu1, menu);
          return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item_red:
+                linear1.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_yellow:
+                linear1.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_blue:
+                linear1.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_rotate:
+                btn1.setRotation(45);
+                return true;
+            case R.id.item_zoom:
+                btn1.setScaleX(2);
+                return true;
+        }
+        //우리가 직접 작성할때 false로 해줌
+        return false;
+
     }
 }
