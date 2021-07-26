8a919c34cb0bdc87f13afde4c7fb2b5af6f08856
iruy <jeon1310>
2021-07-26 10:46:32 +0900

    Edit tost1.xml(Dialog_negative_button Listener) Commit

--- app/src/main/java/kr/hs/emirim/w2015/project7_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/project7_3/MainActivity.java
@@ -7,11 +7,14 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textName, textEmail;
-    View dialogV;
+    TextView textName, textEmail, textToast;
+    View dialogV, toastV;
+    EditText editName, editEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +35,26 @@ public class MainActivity extends AppCompatActivity {
             dialog.setTitle(R.string.btn_input);
             dialog.setIcon(R.drawable.user);
             dialog.setView(dialogV);
-            dialog.setPositiveButton("확인",null);
-            dialog.setNegativeButton("취소",null);
+            dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    editName = dialogV.findViewById(R.id.dialog_edit_name);
+                    editEmail = dialogV.findViewById(R.id.dialog_edit_email);
+                    textName.setText(editName.getText().toString());
+                    textEmail.setText(editEmail.getText().toString());
+                }
+            });
+            dialog.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast toast = new Toast(MainActivity.this);
+                    toastV = View.inflate(MainActivity.this, R.layout.tost1, null);
+                    textToast= toastV.findViewById(R.id.toast_text1);
+                    textToast.setText("취소했습니다");
+                    toast.setView(toastV);
+                    toast.show();
+                }
+            });
             dialog.show();
 
         }
