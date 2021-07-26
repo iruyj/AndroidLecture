986de159df83f624ec79e910a7a580736ac5fe54
iruy <jeon1310>
2021-07-26 11:46:26 +0900

    Edit MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2015/ex11_1_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/ex11_1_2/MainActivity.java
@@ -3,12 +3,28 @@ package kr.hs.emirim.w2015.ex11_1_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String[] arr = {"Black Widow","Bose Baby2","Cruela","Sing","셜록홈즈","쥬만지"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list1 = findViewById(R.id.list1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        list1.setAdapter(adapter);
+
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), arr[i]+"가 선택됨",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
