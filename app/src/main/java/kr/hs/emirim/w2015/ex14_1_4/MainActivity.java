17459c21cb7bf2c7f923007c9a669699f66f64be
iruyj <jeon1310@gmail.com>
2021-08-24 20:17:11 +0900

    MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2015/ex14_1_4/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2015/ex14_1_4/MainActivity.java
@@ -2,13 +2,39 @@ package kr.hs.emirim.w2015.ex14_1_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("서비스(데몬) 연습");
+
+        intent = new Intent(this, MusicService.class);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnStop = findViewById(R.id.btn_stop);
+
+        btnStart.setOnClickListener(btnListener);
+        btnStop.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_start:
+                    startService(intent);
+                    break;
+                case R.id.btn_stop:
+                    stopService(intent);
+                    break;
+            }
+        }
+    };
 }
